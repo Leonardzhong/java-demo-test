a9506f387fc5b4a7d7a0b152ca96ecac1f307404
leonard <zhya524@gmail.com>
2019-04-18 18:43:13 +0800

    seventh commit

---
+++ src/main/java/com/dailylearn/KthLargeElement.java
@@ -0,0 +1,21 @@
+package com.dailylearn;
+
+import java.util.PriorityQueue;
+
+public class KthLargeElement {
+    public static void main(String[] args) {
+        int[] nums = {99, 99};
+        int k = 1;
+        int res = findKthLargest(nums, k);
+        System.out.println(res);
+    }
+
+    private static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heapQueue = new PriorityQueue<>();
+        for(int num : nums){
+            heapQueue.add(num);
+            if(num > k) heapQueue.poll();
+        }
+        return heapQueue.peek();
+    }
+}
