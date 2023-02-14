bc505979cbbfb312975e8863c916d0a09d7f309a
leonard <zhya524@gmail.com>
2023-02-14 23:27:22 +0800

    scanner nextLine next

--- java-demo-core/src/main/java/demo/core/leetcode/CombinationSum.java
+++ java-demo-core/src/main/java/demo/core/leetcode/CombinationSum.java
@@ -1,5 +1,7 @@
 package demo.core.leetcode;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,32 +9,41 @@ import java.util.List;
 public class CombinationSum {
 
     public static void main(String[] args) {
-        int[] candidates = new int[]{2, 3};
-        int target = 12;
+        int[] candidates = new int[]{2, 3, 6, 7};
+        int target = 7;
         List<List<Integer>> res;
         res = combinationSum(candidates, target);
-        System.out.println(res);
+        System.out.println("res=" + res);
     }
 
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> list = new ArrayList<List<Integer>>();
+
+        //非必需
         Arrays.sort(candidates);
+
         backtrack(list, new ArrayList<>(), candidates, target, 0);
         return list;
 
     }
 
     private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] cand, int remain, int start) {
-        if (remain < 0) return;
+        if (remain < 0) {
+            return;
 
-        else if (remain == 0) list.add(new ArrayList<>(tempList));
+        } else if (remain == 0) {
+            list.add(new ArrayList<>(tempList));
+            System.out.println("====remain==0====" + ",tempList=" + new Gson().toJson(tempList));
 
-        else {
+        } else {
             for (int i = start; i < cand.length; i++) {
                 tempList.add(cand[i]);
+                System.out.println("i=" + i + ",add[i]=" + cand[i] + ",tempList=" + new Gson().toJson(tempList) + ",remain=" + (remain-cand[i]));
 
                 backtrack(list, tempList, cand, remain - cand[i], i);
+
                 tempList.remove(tempList.size() - 1);
+                System.out.println("i=" + i + ",remove[i]=" + cand[i] + ",tempList=" + new Gson().toJson(tempList) + ",remain="+ (remain-cand[i]));
             }
 
         }
